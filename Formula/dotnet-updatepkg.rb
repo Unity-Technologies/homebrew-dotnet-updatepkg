1be1ab623e8a300ead61062f98a3778fafd0dd26
Henrik Ravn <hravnx@gmail.com>
2018-02-15 12:34:51 +0100

    Move head URL from HTTPS to SSH

--- Formula/dotnet-updatepkg.rb
+++ Formula/dotnet-updatepkg.rb
@@ -1,6 +1,6 @@
 class DotnetUpdatepkg < Formula
   homepage "https://gitlab.internal.unity3d.com/CDS/dotnet-updatepkg"
-  head "https://gitlab.internal.unity3d.com/CDS/dotnet-updatepkg.git"
+  head "git@gitlab.internal.unity3d.com:CDS/dotnet-updatepkg.git"
 
   def install
     bin.install 'dotnet-updatepackage', 'dotnet-updatepackage.fsx'
